8a48af905c071112d23262f890b96c541d382680
pranjal-prasun-gmp <pranjalofficial1996@gmail.com>
2026-09-08 12:32:28 +0530

    Manually pre-create google_mlkit_commons symlink to fix Xcode Cloud pod install

--- gmp/ios/ci_scripts/ci_post_clone.sh
+++ gmp/ios/ci_scripts/ci_post_clone.sh
@@ -1,36 +1,22 @@
-#!/bin/sh
-set -ex
-
-# Ensure Homebrew and CocoaPods binaries are in PATH
-export PATH="/opt/homebrew/bin:/usr/local/bin:$PATH"
-
-FLUTTER_ROOT="$HOME/flutter"
-PROJECT_ROOT="$CI_PRIMARY_REPOSITORY_PATH/gmp"
-
-# Install Flutter if not already present
-if [ ! -d "$FLUTTER_ROOT" ]; then
-    git clone https://github.com/flutter/flutter.git --depth 1 -b stable "$FLUTTER_ROOT"
-fi
-
-export PATH="$FLUTTER_ROOT/bin:$PATH"
-
 cd "$PROJECT_ROOT"
 
-# Precache iOS dependencies
+rm -rf ios/Pods ios/.symlinks ios/Podfile.lock .dart_tool build
+flutter clean
 flutter precache --ios
-
-# Clean stale caches
-rm -rf ios/Pods
-rm -rf ios/.symlinks
-rm -rf ios/Podfile.lock
-rm -rf .dart_tool
-
-# Get Flutter packages
 flutter pub get
 
-# Generate native iOS project files & plugin symlinks without a full build
-flutter build ios --config-only --no-codesign
+# Workaround: pre-create the .symlinks path for google_mlkit_commons
+# so Flutter's podhelper.rb can find apple_silicon_simulator.rb reliably
+MLKIT_COMMONS_VERSION=$(grep -A2 "name: google_mlkit_commons" pubspec.lock | grep version | sed 's/.*"\(.*\)".*/\1/')
+MLKIT_COMMONS_PATH="$HOME/.pub-cache/hosted/pub.dev/google_mlkit_commons-${MLKIT_COMMONS_VERSION}"
 
-# Run pod install
+mkdir -p ios/.symlinks/plugins
+if [ -d "$MLKIT_COMMONS_PATH" ]; then
+  ln -sfn "$MLKIT_COMMONS_PATH" "ios/.symlinks/plugins/google_mlkit_commons"
+fi
+
 cd ios
 pod install
+cd "$PROJECT_ROOT"
+
+flutter build ios --release --no-codesign
